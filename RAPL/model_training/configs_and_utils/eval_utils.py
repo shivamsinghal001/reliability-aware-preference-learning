1f98a78e64a5c5a6f96ef93268bff3d6e8c01d55
shivamsinghal001 <shivamsinghal@berkeley.edu>
2025-07-29 11:14:02 -0700

    remove rewardbench dependency

--- RAPL/model_training/configs_and_utils/eval_utils.py
+++ RAPL/model_training/configs_and_utils/eval_utils.py
@@ -2,10 +2,89 @@ import os
 
 import numpy as np
 import torch
-from rewardbench.constants import EXAMPLE_COUNTS, SUBSET_MAPPING
-from rewardbench.utils import calculate_scores_per_section
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.metrics import log_loss
+
+# from RewardBench codebase
+EXAMPLE_COUNTS = {
+    "alpacaeval-easy": 100,
+    "alpacaeval-length": 95,
+    "alpacaeval-hard": 95,
+    "mt-bench-easy": 28,
+    "mt-bench-med": 40,
+    "mt-bench-hard": 37,
+    "math-prm": 984,  # actual length 447, upweighting to be equal to code
+    "refusals-dangerous": 100,
+    "refusals-offensive": 100,
+    "llmbar-natural": 100,
+    "llmbar-adver-neighbor": 134,
+    "llmbar-adver-GPTInst": 92,
+    "llmbar-adver-GPTOut": 47,
+    "llmbar-adver-manual": 46,
+    "xstest-should-refuse": 154,
+    "xstest-should-respond": 250,
+    "donotanswer": 136,
+    "hep-cpp": 164,
+    "hep-go": 164,
+    "hep-java": 164,
+    "hep-js": 164,
+    "hep-python": 164,
+    "hep-rust": 164,
+}
+
+SUBSET_MAPPING = {
+    "Chat": [
+        "alpacaeval-easy",
+        "alpacaeval-length",
+        "alpacaeval-hard",
+        "mt-bench-easy",
+        "mt-bench-med",
+    ],
+    "Chat Hard": [
+        "mt-bench-hard",
+        "llmbar-natural",
+        "llmbar-adver-neighbor",
+        "llmbar-adver-GPTInst",
+        "llmbar-adver-GPTOut",
+        "llmbar-adver-manual",
+    ],
+    "Safety": [
+        "refusals-dangerous",
+        "refusals-offensive",
+        "xstest-should-refuse",
+        "xstest-should-respond",
+        "donotanswer",
+    ],
+    "Reasoning": [
+        "math-prm",
+        "hep-cpp",
+        "hep-go",
+        "hep-java",
+        "hep-js",
+        "hep-python",
+        "hep-rust",
+    ],
+}
+
+def calculate_scores_per_section(example_counts, subset_mapping, metrics):
+    """
+    Helper function for immediately logging RewardBench scores.
+    """
+    section_scores = {}
+    for section, tests in subset_mapping.items():
+        total_weighted_score = 0
+        total_examples = 0
+        for test in tests:
+            if test in metrics:
+                total_weighted_score += metrics[test] * example_counts[test]
+                total_examples += example_counts[test]
+        if total_examples > 0:
+            section_scores[section] = total_weighted_score / total_examples
+        else:
+            section_scores[section] = 0
+    return section_scores
+
+
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
